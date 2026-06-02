e665e5a2a4749578beacfdebb24ed8d9af3626d7
Duidui979 <zhoushuaiyu0319@163.com>
2026-06-02 15:47:56 +1000

    Blend Perlin noise into building height data

--- js/random-mechanic.js
+++ js/random-mechanic.js
@@ -7,8 +7,15 @@ class RandomMechanic {
   }
 
   setup() {
-    this.heightGrid = Array.from({ length: this.rows }, () =>
-      Array.from({ length: this.columns }, () => Math.random()),
+    const noiseOffset = Math.random() * 1000;
+
+    this.heightGrid = Array.from({ length: this.rows }, (_, row) =>
+      Array.from({ length: this.columns }, (_, column) => {
+        const randomHeight = Math.random();
+        const noiseHeight = noise(noiseOffset + column * 0.18, noiseOffset + row * 0.18);
+
+        return constrain(randomHeight * 0.45 + noiseHeight * 0.55, 0, 1);
+      }),
     );
   }
 
